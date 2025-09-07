4c8f0740ec3d7f93158e4cf5b226dbef290ec951
jacobot1 <155650471+jacobot1@users.noreply.github.com>
2025-09-07 00:57:22 -0500

    Add project files.

---
+++ InteractionControl/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using GameNetcodeStuff;
+using UnityEngine.InputSystem;
+
+namespace InteractionControl.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Interact_performed")]
+        [HarmonyPrefix]
+        static bool GrabWithJetpackPatch(PlayerControllerB __instance, ref InputAction.CallbackContext context)
+        {
+            // Knock back player
+            if (InteractionControlMod.configGrabWithJetpack.Value)
+            {
+                if (__instance.IsOwner && __instance.isPlayerDead && (!__instance.IsServer || __instance.isHostPlayerObject))
+                {
+                    if (!StartOfRound.Instance.overrideSpectateCamera && __instance.spectatedPlayerScript != null && !__instance.spectatedPlayerScript.isPlayerDead)
+                    {
+                        __instance.SpectateNextPlayer();
+                    }
+                }
+                else
+                {
+                    if (((!__instance.IsOwner || !__instance.isPlayerControlled || (__instance.IsServer && !__instance.isHostPlayerObject)) && !__instance.isTestingPlayer) || !context.performed || __instance.timeSinceSwitchingSlots < 0.2f || __instance.inSpecialMenu)
+                    {
+                        return false;
+                    }
+                    ShipBuildModeManager.Instance.CancelBuildMode();
+                    if (!__instance.isGrabbingObjectAnimation && !__instance.isTypingChat && !__instance.inTerminalMenu && !__instance.throwingObject && !__instance.IsInspectingItem && !(__instance.inAnimationWithEnemy != null) && (__instance.jetpackControls || InteractionControlMod.configGrabWithJetpack.Value) && (__instance.disablingJetpackControls || InteractionControlMod.configGrabWithJetpack.Value) && !StartOfRound.Instance.suckingPlayersOutOfShip)
+                    {
+                        if (!__instance.activatingItem && !__instance.waitingToDropItem)
+                        {
+                            __instance.BeginGrabObject();
+                        }
+                        if (!(__instance.hoveringOverTrigger == null) && !__instance.hoveringOverTrigger.holdInteraction && (!__instance.isHoldingObject || __instance.hoveringOverTrigger.oneHandedItemAllowed) && (!__instance.twoHanded || (__instance.hoveringOverTrigger.twoHandedItemAllowed && !__instance.hoveringOverTrigger.specialCharacterAnimation)) && __instance.InteractTriggerUseConditionsMet())
+                        {
+                            __instance.hoveringOverTrigger.Interact(__instance.thisPlayerBody);
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ InteractionControl/Plugin.cs
@@ -0,0 +1,52 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Logging;
+using InteractionControl.Patches;
+using BepInEx.Configuration;
+
+namespace InteractionControl
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInDependency("com.rune580.LethalCompanyInputUtils", BepInDependency.DependencyFlags.HardDependency)]
+    public class InteractionControlMod : BaseUnityPlugin
+    {
+        // Mod metadata
+        public const string modGUID = "com.jacobot5.InteractionControl";
+        public const string modName = "InteractionControl";
+        public const string modVersion = "1.0.0";
+
+        // Initalize Harmony
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        // Create static instance
+        private static InteractionControlMod Instance;
+
+        // Configuration
+        public static ConfigEntry<bool> configGrabWithJetpack;
+
+        // Initialize logging
+        public static ManualLogSource mls;
+
+        private void Awake()
+        {
+            // Ensure static instance
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            // Send alive message
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("InteractionControl has awoken.");
+
+            // Bind configuration
+            configGrabWithJetpack = Config.Bind("General.Toggles",
+                                                "GrabWithJetpack",
+                                                false,
+                                                "Whether to enable grabbing objects while in jetpack control mode");
+            // Do the patching
+            harmony.PatchAll(typeof(InteractionControlMod));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
